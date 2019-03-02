c9b8dcb6dd86733cdbbd23dd104ab992ad85bd40
Jongseong Woo <22februa@gmail.com>
2019-03-02 22:39:42 +0900

    revert popularity.sh

--- popularity.sh
+++ popularity.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-NUM_QUERY=2
-TOPK_FILEPATH=data/query.top$NUM_QUERY.json
+QUERY="all"
+TOPK_FILEPATH=data/blouse.top40.txt
 NUM_REVIEWS=5
 NUM_JJIM=10
-MAX_PAGING_INDEX=2
+MAX_PAGING_INDEX=6
+CHROMEDRIVE_PATH=resources/chromedriver_win32/chromedriver.exe
 
-python popularity/get_rank_topk.py \
-  --num_query $NUM_QUERY \
-  --topk_filepath $TOPK_FILEPATH \
-
-# python -m popularity.popularity \
-#   --topk_filepath $TOPK_FILEPATH \
-#   --num_reviews $NUM_REVIEWS \
-#   --num_jjim $NUM_JJIM \
-#   --max_paging_index $MAX_PAGING_INDEX \
-#   --chromedrive_path $CHROMEDRIVE_PATH
+python -m popularity.popularity \
+  --query="$QUERY" \
+  --num_reviews=$NUM_REVIEWS \
+  --num_jjim=$NUM_JJIM \
+  --max_paging_index=$MAX_PAGING_INDEX \
+  --topk_filepath=$TOPK_FILEPATH \
+  --chromedrive_path=$CHROMEDRIVE_PATH
